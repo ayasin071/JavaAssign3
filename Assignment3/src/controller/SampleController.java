a38703560dd7a6bbd338b2828da20aadc6e1458a
Ahmad Yasin <113630859+ayasin071@users.noreply.github.com>
2023-04-03 13:49:10 -0600

    Add files via upload
    combined assign 2 and 3

---
+++ Assignment3/src/controller/SampleController.java
@@ -0,0 +1,204 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
+
+public abstract class SampleController {
+    public static void main(String[] args) {
+       
+    	 StoreManager storeManager = new StoreManager();
+         try {
+			storeManager.loadData();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    
+       
+    }
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnRemove;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private ComboBox<?> cbCategory1;
+
+    @FXML
+    private ListView<?> lvHome;
+
+    @FXML
+    private ToggleGroup rb1;
+
+    @FXML
+    private RadioButton rbName;
+
+    @FXML
+    private RadioButton rbSerNum;
+
+    @FXML
+    private RadioButton rbType;
+
+    @FXML
+    private Tab tabAddToy;
+
+    @FXML
+    private Tab tabHome;
+
+    @FXML
+    private Tab tabRemToy;
+
+    @FXML
+    private TextField tfAgeAppr;
+
+    @FXML
+    private TextField tfAvailCount;
+
+    @FXML
+    private TextField tfBrand;
+
+    @FXML
+    private TextField tfClass;
+
+    @FXML
+    private TextField tfDesigner;
+
+    @FXML
+    private TextField tfMaterial;
+
+    @FXML
+    private TextField tfMaxPlayers;
+
+    @FXML
+    private TextField tfMinPlayers;
+
+    @FXML
+    private TextField tfName;
+
+    @FXML
+    private TextField tfPrice;
+
+    @FXML
+    private TextField tfSN;
+
+    @FXML
+    private TextField tfSN1;
+
+    @FXML
+    private TextField tfSerNum;
+
+    @FXML
+    private TextField tfSize;
+
+    @FXML
+    private TextField tfType;
+
+    @FXML
+    private Text txtAgeAppr;
+
+    @FXML
+    private Text txtAnimal;
+
+    @FXML
+    private Text txtAvailCount;
+
+    @FXML
+    private Text txtBoardGame;
+
+    @FXML
+    private Text txtBrand;
+
+    @FXML
+    private Text txtCategory;
+
+    @FXML
+    private Text txtClass;
+
+    @FXML
+    private Text txtDesigner;
+
+    @FXML
+    private Text txtEnter;
+
+    @FXML
+    private Text txtEnterSN;
+
+    @FXML
+    private Text txtFig;
+
+    @FXML
+    private Text txtMaterial;
+
+    @FXML
+    private Text txtMaxPlayers;
+
+    @FXML
+    private Text txtMinPlayers;
+
+    @FXML
+    private Text txtName;
+
+    @FXML
+    private Text txtPrice;
+
+    @FXML
+    private Text txtPuzzle;
+
+    @FXML
+    private Text txtSN;
+
+    @FXML
+    private Text txtSN1;
+
+    @FXML
+    private Text txtSearch;
+
+    @FXML
+    private Text txtSerNun;
+
+    @FXML
+    private Text txtSize;
+
+    @FXML
+    private Text txtType;
+    
+    @FXML
+    ComboBox<String> cbCategory = new ComboBox<>();
+    @FXML
+    void initialize() {
+    cbCategory.getItems().addAll("Figure", "Animals", "Puzzle", "Board Game");
+    }
+    
+    
+    @FXML
+    void rbNameHandler(ActionEvent event) {
+    	
+
+    }
+
+    @FXML
+    void rbSerHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rbTypeHandler(ActionEvent event) {
+
+    }
+
+}
